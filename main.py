16bb87ef17948ca0875ccda2cbd0f6f750e4db9d
CodeSandbox <support@codesandbox.io>
2023-10-22 14:32:06 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 app = Flask(__name__)
 
+# Set your SMTP server details
+SMTP_SERVER = "smtp-relay.brevo.com"
+SMTP_PORT = 587
+SMTP_USERNAME = "sagar.puri@brevo.com"
+SMTP_PASSWORD = "SfOh9REAXgqcxQdy"
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+@app.route('/send_email', methods=['POST'])
+def send_email():
+    to_address = request.form['to_address']
+    from_address = request.form['from_address']
+    subject = request.form['subject']
+    message = request.form['message']
 
-if __name__ == "__main__":
-    app.run()
+    # Create the email message
+    msg = MIMEMultipart()
+    msg['From'] = from_address
+    msg['To'] = to_address
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message, 'plain'))
+
+    try:
+        # Connect to the SMTP server and send the email
+        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        server.starttls()
+        server.login(SMTP_USERNAME, SMTP_PASSWORD)
+        server.sendmail(from_address, to_address, msg.as_string())
+        server.quit()
+        return "Email sent successfully!"
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
